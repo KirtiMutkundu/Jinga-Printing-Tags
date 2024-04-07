ef45c9c4992674446872022cffa7272f64329814
KirtiMutkundu <kirtimutkundureddy2007@gmail.com>
2024-04-08 01:12:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def send_data (request):
+    d={'name':'KIRTI', 'age':23}
+    return render (request, 'send_data.html', context=d)
+
